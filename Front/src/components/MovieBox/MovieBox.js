d4497fcad47a51b643820886dd2c3721163c03cf
ugnead <u.adomaityte98@gmail.com>
2023-05-25 16:33:39 +0300

    add front end functionality

---
+++ Front/src/components/MovieBox/MovieBox.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import MovieBoxCSS from './MovieBox.module.css';
+
+function MovieBox(props) {
+    let {id, image, title, handleMovieBoxClick} = props;
+
+    const handleClick = () => {
+        handleMovieBoxClick(id);
+      };
+
+    return (
+        <>
+            <div onClick={handleClick} className={`m-1 ${MovieBoxCSS.box}`}>
+                <img className={`rounded-1 ${MovieBoxCSS.img}`} src={image} alt={title} />
+                <div>{title}</div>
+            </div>
+        </>
+        
+    )
+}
+
+export default MovieBox
